851166a47294cf6d3a28304cd993bba22b2063d4
Heath <hlberg2000@gmail.com>
2019-11-29 21:59:24 -0500

    1.2 Image Basics
    All the information from the Image Basics Chapter.

--- 1.2_image_basics/image_basics.py
+++ 1.2_image_basics/image_basics.py
@@ -1,10 +1,16 @@
+# import the necessary packages
 import argparse
 import cv2
-
+ 
+# construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True. help="Path to the Image.")
+ap.add_argument("-i", "--image", required=True, help="Path to the image")
 args = vars(ap.parse_args())
-
+ 
+# load the image, grab its dimensions, and show it
+print('Args:', args["image"])
 image = cv2.imread(args["image"])
-(h,w) = image.shape[:2]
-cv2.show("Original", image)
+(h, w) = image.shape[:2]
+cv2.imshow("Original", image)
+
+print((h,w))
